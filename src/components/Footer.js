2e257c13d7f9ee3f6c2c59a5edd29d48c235586b
Surya_Chandra <135944174+suryachandra-dev@users.noreply.github.com>
2024-03-06 10:13:14 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,13 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <footer className="bg-gray-800 text-white py-3 text-center"><span className='text-lg hover:text-gray-400 transition duration-300'>Created by <span className='font-bold'>Surya Chandra </span></span><span className='text-sm hover:text-gray-400 transition duration-300'> &copy; 2024 March <span className=' font-bold'>Quick</span><span className=' font-bold hover:text-red-500'>Bite</span>
+        <footer className="bg-gray-800 text-white py-3 text-center fixed bottom-0 w-full"><span className='text-lg hover:text-gray-400 transition duration-300'>Created by <span className='font-bold'>Surya Chandra </span></span><span className='text-sm hover:text-gray-400 transition duration-300'> &copy; 2024 March <span className=' font-bold'>Quick</span><span className=' font-bold hover:text-red-500'>Bite</span>
             </span> </footer>
     </div>
   )
 }
 
 export default Footer;
+
+
+
